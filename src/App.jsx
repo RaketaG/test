d2f890a2bfb2aa4a779afc9633732b2c6c60cb7b
Giorguna <Giorguna@Goginashvili.com>
2026-01-28 18:14:41 +0400

    Added features: Spining Loader, Cache, Abort Controller, Http Error Handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,45 @@
   3. Display on screen all the information about product.
 */
 
-import { useState, useEffect } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
+  const cacheData = useRef({});
+  const abortController = useRef(null);
   const [productData, setProductData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const randomProductGenerator = () => {
     const randomNumber = Math.floor(Math.random() * 20) + 1;
 
-    fetch(`https://fakestoreapi.com/products/${randomNumber}`)
-      .then((response) => response.json())
-      .then((data) => setProductData(data));
+    if (randomNumber in cacheData.current) {
+      setProductData(cacheData.current[randomNumber]);
+    } else {
+      abortController.current && abortController.current.abort();
+
+      abortController.current = new AbortController();
+
+      setIsLoading(true);
+      fetch(`https://fakestoreapi.com/products/${randomNumber}`, {
+        signal: abortController.current.signal
+      })
+        .then((response) => {
+          if (!response.ok)
+            throw new Error(`Something went wrong!\nHTTP: ${response.status}`);
+          return response.json();
+        })
+        .then((data) => {
+          setProductData(data);
+          cacheData.current[randomNumber] = data;
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          error.name === 'AbortError' ? console.log("Gocha") : 
+          alert(error);
+          setIsLoading(false);
+        });
+    }
   };
 
   return (
@@ -25,18 +52,22 @@ function App() {
       >
         Generate Random Product
       </button>
-      {Object.entries(productData).map(([key, value]) => (
-        <div key={key} className='infoContainer'>
-          <p className='infoKey'><strong>{key}: </strong></p>
-          <p className='infoValue'>
-            {(typeof value === "object" && value !== null)
-              ? Object.entries(value)
-                .map(([key, val]) => `${key}: ${val}`)
-                .join(" | ")
-              : value}
-          </p>
-        </div>
-      ))}
+
+      {isLoading ?
+        <div className="spinner" /> :
+        Object.entries(productData).map(([key, value]) => (
+          <div key={key} className='infoContainer'>
+            <p className='infoKey'><strong>{key}: </strong></p>
+            <p className='infoValue'>
+              {(typeof value === "object" && value !== null)
+                ? Object.entries(value)
+                  .map(([key, val]) => `${key}: ${val}`)
+                  .join(" | ")
+                : value}
+            </p>
+          </div>
+        ))
+      }
     </>
   )
 }
